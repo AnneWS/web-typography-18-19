58015e56b3f9e35026cc206f35facab312cd87c7
AnneWS <32013118+AnneWS@users.noreply.github.com>
2019-05-22 19:43:17 +0200

    Add files via upload

--- closed-captions/sounds.js
+++ closed-captions/sounds.js
@@ -1,15 +1,27 @@
 // Hier kan je tijden toevoegen in secondes
 // Op die mommenten komt er een class op de body.
 sounds = [
-	0,
-	9,
-	12,
-	15,
-	16.5,
-	24,
-	25.8,
-	31,
-	32,
-	34,
-	86
+    0,
+    1.5,
+    9,
+    12,
+    15,
+    16.5,
+    24,
+    25.8,
+    31,
+    32,
+    34,
+    86,
+    88,
+    91,
+    96,
+    99,
+    103,
+    111,
+    113,
+    114,
+    115,
+    130,
+    136
 ];
