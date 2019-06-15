24d65a22f41efdce83445729db3ebf4a0d1f9e90
AnneWS <32013118+AnneWS@users.noreply.github.com>
2019-06-15 11:04:51 +0200

    Add files via upload

---
+++ versie2/sounds.js
@@ -0,0 +1,30 @@
+// Hier kan je tijden toevoegen in secondes
+// Op die mommenten komt er een class op de body.
+sounds = [
+    0,
+    1.5,
+    9,
+    12,
+    15,
+    16.5,
+    20,
+    24,
+    24.5,
+    25.8,
+    31,
+    32,
+    34,
+    75,
+    86,
+    88,
+    91,
+    96,
+    99,
+    103,
+    111,
+    113,
+    114,
+    115,
+    130,
+    136
+];
